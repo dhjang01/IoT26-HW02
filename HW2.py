3da4bb67d22b8e06a1614c6a1a7ba40eb41bf6f4
LIM_K_M <navymook7608@naver.com>
2026-05-11 00:39:09 +0900

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -4,7 +4,9 @@ from signal import pause
 led = LED(14)
 button = Button(4)
 
+# LED ON when pressed
 button.when_pressed = led.on
+# LED OFF when unpressed
 button.when_released = led.off
 
 pause()
